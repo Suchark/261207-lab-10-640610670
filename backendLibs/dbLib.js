9d36540e2f3bc2862977b50a62c197b0b62bdeed
Your Name <you@example.com>
2022-09-22 22:48:52 +0700

    okk

--- backendLibs/dbLib.js
+++ backendLibs/dbLib.js
@@ -1,12 +1,12 @@
-import fs from `fs`;
+import fs from "fs";
 
-export function readDB(){
-  const srt = fs.readFileSync("db/chatroom.json",{encoding: "utf-8"});
+export function readDB() {
+  const srt = fs.readFileSync("db/chatroom.json", { encoding: "utf-8" });
   const chatroom = JSON.parse(srt);
   return chatroom;
 }
 
-export function writeDB(chatroom){
+export function writeDB(chatroom) {
   const str = JSON.stringify(chatroom);
-  fs.writeFileSync("db/chatroom.json", srt, { encoding: "utf-8"})
+  fs.writeFileSync("db/chatroom.json", str, { encoding: "utf-8" });
 }
